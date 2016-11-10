0143941661bc5b6c4a490d4560382e507996e1a4
Julia <juliageist23@gmail.com>
2016-11-09 22:17:36 -0800

    workspace with rxswift

---
+++ Pods/Target Support Files/Pods-FunctionalReactiveProgrammingTutorial/Pods-FunctionalReactiveProgrammingTutorial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FunctionalReactiveProgrammingTutorial : NSObject
+@end
+@implementation PodsDummy_Pods_FunctionalReactiveProgrammingTutorial
+@end
---
+++ Pods/Target Support Files/Pods-FunctionalReactiveProgrammingTutorial/Pods-FunctionalReactiveProgrammingTutorial-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_FunctionalReactiveProgrammingTutorialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FunctionalReactiveProgrammingTutorialVersionString[];
+
